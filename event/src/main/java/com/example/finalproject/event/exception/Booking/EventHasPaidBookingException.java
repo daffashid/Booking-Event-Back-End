94bcf6b87ee09dba4b80334855ef371f58698401
Daffa <daffashid28@gmail.com>
2026-01-14 21:16:10 +0700

    fixing error message

--- event/src/main/java/com/example/finalproject/event/exception/Booking/EventHasPaidBookingException.java
+++ event/src/main/java/com/example/finalproject/event/exception/Booking/EventHasPaidBookingException.java
@@ -3,6 +3,6 @@ package com.example.finalproject.event.exception.Booking;
 public class EventHasPaidBookingException extends RuntimeException {
 
     public EventHasPaidBookingException() {
-        super("Event cannot be deleted because it has paid bookings");
+        super("Event cannot be deleted because it has active bookings");
     }
 }
